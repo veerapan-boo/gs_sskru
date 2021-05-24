53b12ea176850f0463fd9787effcb9fab7e6cf66
benz45 <benz45.th@outlook.com>
2021-05-24 20:42:48 +0700

    Manage admin poster ui

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFFFFD82A);
+const kPrimaryColor = Color(0xFFDFBD69);
 const kDarkBlackColor = Color(0xFF191919);
 const kBgColor = Color(0xFFE7E7E7);
 const kBodyTextColor = Color(0xFF666666);
@@ -8,3 +8,5 @@ const kBodyTextColor = Color(0xFF666666);
 const kDefaultPadding = 20.0;
 const kMaxWidth = 1232.0;
 const kDefaultDuration = Duration(milliseconds: 250);
+
+Color kDisabledPrimaryColor = Color(0xFF919191);
